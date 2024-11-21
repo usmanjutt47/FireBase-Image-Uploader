4c091c84ad2e5a842e5845f6affdc43d57ef1de1
Muhammad Usman <142773789+usmanjutt47@users.noreply.github.com>
2024-11-21 16:19:50 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Button, Image, View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default function App() {
+  const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  image: {
+    width: 200,
+    height: 200
+  }
+});
